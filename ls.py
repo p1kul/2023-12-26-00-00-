249345dcb4a070c0cfcf54224a2c931c8e801e92
p1kul <funikova@me.com>
2024-09-15 11:39:13 +0300

    Add files via upload

--- ls.py
+++ ls.py
@@ -1,11 +1,9 @@
 def introspection_info(obj):
     slovar = {}
     slovar['type'] = type(obj).__name__
-    slovar['attributes'] = dir(obj)
+    slovar['attributes'] =  [x for x in dir(obj) if not callable(getattr(obj, x))]
     slovar['methods'] = [x for x in dir(obj) if callable(getattr(obj, x))]
     slovar['module'] = introspection_info.__module__
     return (slovar)
 number_info = introspection_info(42)
 print(number_info)
-
-
